6805abf9b0ed5941708eba4c8ee1f738a85f201b
Sander Scheijbeler <sander@raakbeleving.nl>
2019-08-03 15:47:04 +0200

    added about page layout, found cool menu hamburger

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,7 +2,36 @@
   <Layout>
 
     <section class="section-about">
-      <h1>About me</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10">
+            <h1 class="mt-5 mb-3">
+              "Now this is a story all about how my life got twisted upside down..."
+            </h1>
+
+            <p class="text__col text__col--two">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam beatae consequatur delectus deleniti distinctio dolore doloremque doloribus dolorum, eaque fugiat illo iste magnam nihil non nostrum officiis omnis perspiciatis placeat quibusdam reprehenderit sed suscipit tempore tenetur ullam veritatis vitae voluptate. Accusantium atque consequatur, eius impedit mollitia porro quaerat. A accusamus adipisci aliquam, beatae blanditiis cum cumque cupiditate deleniti dicta dolore doloribus eaque esse et eveniet incidunt ipsa iure laboriosam laborum libero minima nemo neque nihil numquam odit qui quibusdam rem repudiandae sapiente sequi similique sit velit vero voluptatem! Aliquam cumque, dolorum laborum perferendis quaerat quam rem soluta! Aliquam, doloremque!
+            </p>
+
+            <img class="mb-3" src="/images/uploads/city.jpg" alt="Me!">
+
+            <h2>
+              Maar waarom?
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci commodi, debitis doloremque facere iusto nisi officia placeat ratione reiciendis rem repellat sint suscipit, temporibus. Adipisci consequatur consequuntur corporis deleniti dicta ducimus eligendi fugit incidunt minus nesciunt officiis optio perferendis provident saepe similique tenetur voluptas, voluptates voluptatibus. Aspernatur, dolorum, quidem? Aut corporis deserunt esse hic illum laborum, magni minus nam quis repudiandae sapiente ullam veniam vitae. Ad aperiam autem consectetur consequuntur cum dicta dolores ducimus excepturi illum impedit incidunt iusto laboriosam maxime modi neque nesciunt nihil nulla, pariatur provident repellat rerum sint suscipit vitae voluptas voluptate. Atque ea earum maiores totam!
+            </p>
+
+            <h2>
+              Dus.. wat doe je?!
+            </h2>
+            <p>
+              Ik werk in HTML, (S)CSS, PHP, JavaScript (ES6). <br>
+              Gewoon nog wat termen waar ik mee werk of heb gewerkt: Docker, Bootstrap4, VueJS/NuxtJS, jQuery, Git, twig, Wordpress.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
 
   </Layout>
@@ -16,3 +45,12 @@ export default {
   }
 }
 </script>
+
+<style>
+  .section-about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+</style>
